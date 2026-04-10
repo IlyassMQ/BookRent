d482b4730b13a08ad5956fff13c33cb70c5ac17c
Ilyass Mouqerassou <ilyassmouqerassou0@gmail.com>
2026-04-10 02:24:06 +0100

    status column to users and libraries

--- app/Models/Library.php
+++ app/Models/Library.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Library extends Model
 {
-        protected $fillable = ['name', 'address', 'geo_lat', 'geo_lng', 'user_id'];
+        protected $fillable = ['name', 'address', 'geo_lat', 'geo_lng', 'user_id','status'];
 
     public function user()
     {
